87e7fad126322b04b87c1ccca48cb638475b59ce
Gihoon-Kim <55881595+Gihoon-Kim@users.noreply.github.com>
2021-12-07 18:34:17 -0500

    Update TMA_GetProducts.php

--- php/TMA_GetProducts.php
+++ php/TMA_GetProducts.php
@@ -2,7 +2,7 @@
     $con = mysqli_connect('localhost', 'root', '', 'tma_user');
 
     $statement = mysqli_prepare($con, 
-        "SELECT ProductNumber, ProductName, ProductDesc, ProductPrice, u.UserName
+        "SELECT ProductNumber, ProductName, ProductDesc, ProductPrice, u.UserName, tradeLocation
 	        FROM product p
             JOIN user u
 	        ON p.ProductOwner = u.UserNumber;"
@@ -10,7 +10,7 @@
     mysqli_stmt_execute($statement);
 
     mysqli_stmt_store_result($statement);
-    mysqli_stmt_bind_result($statement, $ProductNumber, $ProductName, $ProductDesc, $ProductPrice, $ProductOwner);
+    mysqli_stmt_bind_result($statement, $ProductNumber, $ProductName, $ProductDesc, $ProductPrice, $ProductOwner, $TradeLocation);
 
     $response = array();
 
@@ -23,7 +23,8 @@
                         'ProductName' => $ProductName,
                         'ProductDesc' => $ProductDesc,
                         'ProductOwner' => $ProductOwner,
-                        'ProductPrice' => $ProductPrice
+                        'ProductPrice' => $ProductPrice,
+                        'TradeLocation' => $TradeLocation
                     ));
     }
 
